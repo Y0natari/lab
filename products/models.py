296ac91c40dc56552f5d2cd8ef82375063c9612a
Artem <33001259+Y0natari@users.noreply.github.com>
2023-12-20 20:03:56 +0200

    Add files via upload

---
+++ products/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    customer_name = models.CharField(max_length=255)
+    customer_email = models.EmailField()
+    order_date = models.DateTimeField(auto_now_add=True)
